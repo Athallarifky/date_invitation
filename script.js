706a8c0dc7028662ff47f3102cb56db5d1e52c32
Athallarifky <103438936+Athallarifky@users.noreply.github.com>
2024-05-27 01:26:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the December Adel :)";
+  question.innerHTML = "YAY SEE U TOMORROW ADELIA HIHIHI :3";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
